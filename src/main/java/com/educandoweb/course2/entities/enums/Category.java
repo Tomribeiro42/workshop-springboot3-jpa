90a13912279cc5098f1ed7caa3748742e2506c79
Tomribeiro42 <tomfribeiro@gmail.com>
2024-02-14 11:17:56 -0300

    Product

--- src/main/java/com/educandoweb/course2/entities/enums/Category.java
+++ src/main/java/com/educandoweb/course2/entities/enums/Category.java
@@ -1,13 +1,18 @@
 package com.educandoweb.course2.entities.enums;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.educandoweb.course2.entities.Product;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -19,6 +24,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long  id;
 	private String name;
+	
+	@Transient 
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -45,6 +53,10 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
 
 	@Override
 	public int hashCode() {
@@ -62,5 +74,7 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 }
